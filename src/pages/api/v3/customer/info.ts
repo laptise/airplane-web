3cd8a66ddb807dd33468d451d4ea33728c0ff61b
Yoonsoo Kim <laptise@live.jp>
2022-02-09 01:03:35 +0900

    決済情報取得

---
+++ src/pages/api/v3/customer/info.ts
@@ -0,0 +1,33 @@
+import { NextApiHandler } from "next";
+import HttpStatusCode from "../../../../httpCodes";
+import { ServerInstance } from "../instance";
+import { getStripeInfoFromToken } from "./authentication";
+
+const getInfo: NextApiHandler = async (req, res) => {
+  const stripe = await getStripeInfoFromToken(req);
+  const customer = await ServerInstance.stripe.customers.retrieve(stripe.stripeId);
+  res.status(200).json(customer);
+};
+
+const update: NextApiHandler = async (req, res) => {
+  const stripe = await getStripeInfoFromToken(req);
+  console.log(JSON.parse(req.body));
+  const customer = await ServerInstance.stripe.customers.retrieve(stripe.stripeId);
+  res.status(200).end();
+};
+
+const handler: NextApiHandler = async (req, res) => {
+  switch (req.method) {
+    case "GET":
+      await getInfo(req, res);
+      break;
+    case "POST":
+      await update(req, res);
+      break;
+    default:
+      res.status(HttpStatusCode.BAD_REQUEST).end();
+      break;
+  }
+};
+
+export default handler;
