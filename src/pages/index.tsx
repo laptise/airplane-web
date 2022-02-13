612356748e274dcd556f73382ddb580d8e93ca40
Yoonsoo Kim <laptise@live.jp>
2022-02-13 21:13:27 +0900

    token exception

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -108,7 +108,7 @@ function Slider() {
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
   try {
     const cookies = nookies.get(ctx); // ブラウザ側で設定したCookieを取得
-    if (!cookies) throw "no Cookie";
+    if (!cookies?.["token"]) throw "no Cookie";
     const { verifyIdToken } = await import("../pages/api/v3/customer/authentication");
     const { uid } = await verifyIdToken(cookies["token"]); // CookieからJWTを取得し検証する
     const { ServerInstance } = await import("./api/v3/instance");
