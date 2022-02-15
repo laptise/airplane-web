a015977a94165aa6b1e10e0236b3c68082aa9d51
Yoonsoo Kim <laptise@live.jp>
2022-02-15 20:34:15 +0900

    add parser

---
+++ src/pages/api/v3/alg.ts
@@ -0,0 +1,11 @@
+import { ServerInstance } from "../../../components/OnServer";
+
+export class Parser {
+  static async getStripeUserFromToken(token: string) {
+    const [uid, hash] = Buffer.from(token, "base64").toString().split(":");
+    console.log(uid, hash);
+    const snapshot = await ServerInstance.firebase.firestore().collection("paymentKeyMaps").where("hash", "==", hash).where("uid", "==", uid).get();
+    const picked = snapshot.docs.map((x) => x.data());
+    return picked[0];
+  }
+}
