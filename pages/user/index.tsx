3b852546f112efc92ad59349c5e8d131ed9edff2
Yoonsoo Kim <laptise@live.jp>
2022-02-15 23:22:39 +0900

    user home tab add

--- pages/user/index.tsx
+++ pages/user/index.tsx
@@ -1,5 +1,7 @@
-import { Stack, TextField } from "@mui/material";
+import { TabContext, TabList, TabPanel } from "@mui/lab";
+import { Stack, Tab, Tabs, TextField } from "@mui/material";
 import { GetServerSideProps } from "next";
+import { useState } from "react";
 import App from "../../components/App";
 import { ServerSideProps } from "../../components/OnServer";
 const SelfBadge: AuthFC = ({ user }) => {
@@ -15,16 +17,44 @@ const SelfBadge: AuthFC = ({ user }) => {
   );
 };
 
+const PremUsers: AuthFC = ({ user }) => {
+  return <Stack className="premUsers"></Stack>;
+};
+
+enum ContextTabs {
+  Feed,
+}
+
+const ContextTab: AuthFC = ({ user }) => {
+  const [value, setValue] = useState("1");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+  return (
+    <Stack className="contextTab">
+      <TabContext value={value}>
+        <TabList onChange={handleChange} aria-label="basic tabs example">
+          <Tab label="フィード" value={"1"} />
+          <Tab label="保存済み" value={"2"} />
+        </TabList>
+        <TabPanel value={"1"}>フィード</TabPanel>
+        <TabPanel value={"2"}>保存済み</TabPanel>
+      </TabContext>
+    </Stack>
+  );
+};
+
 const UserHome: AuthFC = ({ user }) => {
   return (
     <App userName={user?.name} bodyId="userDashboard" title="ホーム">
       <>
         <SelfBadge user={user} />
         <Stack className="searchNewPrem">
-          <TextField label="da" />
+          <TextField size="small" label="認定ユーザーを検索" />
         </Stack>
-        <Stack className="premUsers">hi</Stack>
-        <Stack className="feed">feed</Stack>
+        <PremUsers user={user} />
+        <ContextTab user={user} />
       </>
     </App>
   );
