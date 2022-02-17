69d6542b4ebb267eda2d1eec2b0a7f1b0b3beb66
Yoonsoo Kim <laptise@live.jp>
2022-02-18 00:10:17 +0900

    search with startWith

--- pages/api/v1/premiumUser/index.ts
+++ pages/api/v1/premiumUser/index.ts
@@ -6,11 +6,14 @@ import { checkAuth } from "../customer/authentication";
 const getPremiumUser: NextApiHandler = async (req, res) => {
   const { q } = req.query as { q: string };
   await checkAuth(req, res);
+  const start = q.toLowerCase();
+  const end = start.replace(/.$/, (c) => String.fromCharCode(c.charCodeAt(0) + 1));
   const snapshot = await ServerInstance.firebase
     .firestore()
     .collection("users")
     .where("isPremium", "==", true)
-    .where("lcName", "==", q.toLowerCase())
+    .where("lcName", ">=", start)
+    .where("lcName", "<", end)
     .get();
   const data = snapshot.docs.map((x) => x.data());
   res.status(HttpStatusCode.OK).json(data);
